8b2ec406bde0341778a853d5ae09b0d841ac2ccb
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2017-05-12 21:11:08 +0100

    Create chat_defcon.user.js

---
+++ stackexchange/mod/chat_defcon.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Chatroom DEFCON
+// @namespace   https://charcoal-se.org/
+// @description Gives you an indication of the number of problems caused by users in this room.
+// @author      ArtOfCode
+// @version     0.0.1
+// @updateURL   https://github.com/ArtOfCode-/Userscripts/raw/master/stackexchange/mod/chat_defcon.user.js
+// @downloadURL https://github.com/ArtOfCode-/Userscripts/raw/master/stackexchange/mod/chat_defcon.user.js
+// @supportURL  https://github.com/ArtOfCode-/Userscripts/issues
+// @match       *://chat.stackexchange.com/rooms/*
+// @grant       none
+// ==/UserScript==
+
+$(document).ready(function() {
+  $("#roomname").after($("<span></span>").css({
+    'text-shadow': 'none',
+    'font-size': '12px',
+    'background': '#ff6100',
+    'color': 'white',
+    'padding': '5px 8px',
+    'border-radius': '3px'
+  }).addClass('defcon'));
+  
+  var total = 0;
+  CHAT.RoomUsers.all().forEach(function(u) {
+    $.get('/admin/annotations/' + a.id).done(function(data) {
+      total += $('#annotlist li', data).length;
+      $('.defcon').text(total);
+    });
+  });
+});
