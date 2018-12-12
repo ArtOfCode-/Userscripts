bc2e48d00eff06ea368f0beb0e390be29219e363
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2018-12-12 13:00:59 +0000

    Create knitting_colors.user.js

---
+++ stackexchange/nonmod/knitting_colors.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Knitting Editor Colors
+// @description Add more colors to the SE Winter Bash Knitting Editor.
+// @namespace   https://charcoal-se.org/
+// @author      ArtOfCode
+// @match       https://winterbash2018.stackexchange.com/
+// @grant       none
+// @run-at      document-start
+// @version     0.0.1
+// @updateURL   https://github.com/ArtOfCode-/Userscripts/raw/master/stackexchange/nonmod/knitting_colors.user.js
+// @downloadURL https://github.com/ArtOfCode-/Userscripts/raw/master/stackexchange/nonmod/knitting_colors.user.js
+// ==/UserScript==
+
+const customJQuery = function (f) {
+  if (arguments.length === 1 && typeof(f) === "function") {
+    let source = f.toSource();
+    source = source.replace('["#272727","#90d7f4","#18459e","#346db4","#5353b2","#ed1c24","#ec93e7","#e68d20","#c69c6d","#fece05","#8cc63f","#ffffff"]',
+                            '["#272727","#757575","#c3c3c3","#90d7f4","#18459e","#346db4","#5353b2","#ed1c24","#ec93e7","#e68d20","#c69c6d","#81501a","#fece05","#3a990f","#8cc63f","#aae09a","#ffffff"]');
+    arguments[0] = eval(source);
+  }
+
+  return jQuery.apply(jQuery, arguments);
+};
+
+Object.defineProperty(window, "$", {
+  get: function () {
+    return customJQuery;
+  },
+
+  set: function (jq) {
+    Object.keys(jq).forEach(key => {
+      window.$[key] = jq[key];
+    });
+  }
+});
