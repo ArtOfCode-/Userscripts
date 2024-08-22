d021bf2e168550d741200091e93dcc865c1e34ad
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2024-08-22 12:52:39 +0100

    Add LAS WFDB Export

---
+++ las-wfdb-export.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         WFDB: Export
+// @namespace    http://data/*
+// @version      2024-08-22
+// @description  Make WFDB reports exportable - press Ctrl-Alt-C.
+// @author       You
+// @match        http://data/*
+// @match        https://portal.londonambulance.nhs.uk/Data/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', ev => {
+        if (ev.keyCode === 67 && ev.ctrlKey && ev.altKey) {
+            const reportRows = Array.from(document.querySelectorAll('table table table table table table tr'));
+            const data = [];
+            reportRows.slice(1).forEach(row => {
+                const rowData = [];
+                Array.from(row.children).slice(1).forEach(cell => {
+                    rowData.push(`"${cell.innerText}"`);
+                });
+                data.push(rowData.join(','));
+            });
+            console.log(data.join('\n'));
+        }
+    });
+})();
